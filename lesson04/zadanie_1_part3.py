b85dacb8c74bc6a539e92e73ba456e906535c821
Anatoliy Shishkin <shishkinanatoli@gmail.com>
2019-04-07 20:46:12 +0400

    Оптимизация алгоритма: переход от рекурсивного в итерируемый расчет. Результат: появилась возможность рассчитывать f при n > 495, для сравнения при n = 10000, время исполнения 0,005 секунды, т.е. прирост производительности ещё примерно в 20 раз.

--- lesson04/zadanie_1_part3.py
+++ lesson04/zadanie_1_part3.py
@@ -1,31 +1,35 @@
 __author__ = 'Шишкин Анатолий Васильевич'
 
 import cProfile
+from functools import wraps
 
 
 def memorize(func):
-    def g(n, memory={}):
-        r = memory.get(n)
-        if r is None:
-            r = func(n)
-            memory[n] = r
-        return r
-    return g
+    @wraps(func)
+    def f(n, memory=[0, 1]):
+        '''
+        без декоратора время исполнения при n = 30 составляет 1,45
+        с декоратором memorize время исполнения при n = 490 составляет 0,004, при значении выше выдаёт ошибку рекурсии
+        оптимизировав алгоритм с рекурсивного на итерируемый, удалось запусть программу на n > 500, для сравнения:
+            время исполнения при n = 10000 составляет 0,005, т.е. прирост производительности ещё примерно в 20 раз
+        '''
+        if n < 2:
+            return n
+        pp = 0
+        p = 1
+        for i in range(n - 1):
+            pp, p = p, pp + p
+        return p
+    return f
 
 
 @memorize
-def f(n):
-    '''
-    без декоратора время исполнения при n = 30 составляет 1,45
-    с декоратором memorize время исполнения при n = 490 составляет 0,004, при значении выше выдаёт ошибку рекурсии
-    '''
-    if n < 2:
-        return n
-    return f(n - 1) + f(n - 2)
+def decorated_f(n):
+    return decorated_f(n - 1) + decorated_f(n - 2)
 
 
 def main():
-    print(f(490), end=' ')
+    decorated_f(10000)
 
 
 if __name__ == '__main__':
