b4579007f3528f2342d664c419674347e89295b2
Anatoliy Shishkin <shishkinanatoli@gmail.com>
2019-04-07 20:00:40 +0400

    Вычисляем числа Фибоначчи с созданным дополнительно декоратором memorize. При n = 490, время исполнения 0,004 секунды.

--- lesson04/zadanie_1_part3.py
+++ lesson04/zadanie_1_part3.py
@@ -3,9 +3,21 @@ __author__ = 'Шишкин Анатолий Васильевич'
 import cProfile
 
 
+def memorize(func):
+    def g(n, memory={}):
+        r = memory.get(n)
+        if r is None:
+            r = func(n)
+            memory[n] = r
+        return r
+    return g
+
+
+@memorize
 def f(n):
     '''
     без декоратора время исполнения при n = 30 составляет 1,45
+    с декоратором memorize время исполнения при n = 490 составляет 0,004, при значении выше выдаёт ошибку рекурсии
     '''
     if n < 2:
         return n
@@ -13,7 +25,7 @@ def f(n):
 
 
 def main():
-    print(f(30), end=' ')
+    print(f(490), end=' ')
 
 
 if __name__ == '__main__':
